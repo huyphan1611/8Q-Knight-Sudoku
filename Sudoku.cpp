5e9a72c7cf409b373a5300fc992ed909f7ee3fa4
Huy Phan <pmhuy.us.tk@gmail.com>
2021-09-12 20:19:54 +0700

    output available table Sudoku

--- Sudoku.cpp
+++ Sudoku.cpp
@@ -58,6 +58,18 @@ public:
 
     bool SolveSudoku()
     {
+        /*cout << "0, 0, 0, 0, 0, 0, 0, 0, 2" << endl;
+        cout << "0, 0, 0, 0, 0, 0, 9, 4, 0" << endl;
+        cout << "0, 0, 3, 0, 0, 0, 0, 0, 5" << endl;
+        cout << "0, 9, 2, 3, 0, 5, 0, 7, 4" << endl;
+        cout << "8, 4, 0, 0, 0, 0, 0, 0, 0" << endl;
+        cout << "0, 6, 7, 0, 9, 8, 0, 0, 0" << endl;
+        cout << "0, 0, 0, 7, 0, 6, 0, 0, 0" << endl;
+        cout << "0, 0, 0, 9, 0, 0, 0, 2, 0" << endl;
+        cout << "4, 0, 8, 5, 0, 0, 3, 6, 0" << endl;*/
+        
+        Xuat();
+
         if (SolveSudoku(this->Table) == true)
         {
             cout << endl
